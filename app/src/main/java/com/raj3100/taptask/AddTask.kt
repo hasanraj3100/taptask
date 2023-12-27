9517b3734ed0070dc3baadab411764afc05ea7b4
Raj <hasanraj3100@gmail.com>
2023-12-27 14:46:47 +0600

    priority-dropdown fixed on add task page

--- app/src/main/java/com/raj3100/taptask/AddTask.kt
+++ app/src/main/java/com/raj3100/taptask/AddTask.kt
@@ -1,6 +1,8 @@
 package com.raj3100.taptask
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.ComponentActivity
 import com.raj3100.taptask.R
 
@@ -8,5 +10,10 @@ class AddTask : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_task)
+
+        val spinner = findViewById<Spinner>(R.id.spinnder)
+        val arr = arrayOf("Priority: Low", "Priority: Medium", "Priority: High")
+        val adpt = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, arr)
+        spinner.adapter = adpt
     }
 }
